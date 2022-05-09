60e338ce664802b9c9a237f9b7e0c248f0b3921d
KataokaNagi <calm1836@gmail.com>
2022-05-10 00:44:28 +0900

    add: copy mvp from title to ingame & rename class

---
+++ URPTemplate/Assets/Scripts/UI/Ingame/IngameUIEvent.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// @file     IngameUIEvent.cs
+/// @brief    Define events type that need a parameter.
+/// @auther   Kataoka
+/// @date     2022-05-05 22:22:11
+/// @version  1.0
+/// @history  add
+/// @see      [UnityにおけるMVPパターンについて](https://virtualcast.jp/blog/2019/11/mvp_pattern_on_unity/)
+/// </summary>
+
+using UnityEngine.Events;

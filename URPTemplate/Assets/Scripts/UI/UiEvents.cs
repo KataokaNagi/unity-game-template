3f3f7ea3fd84965504808bacfbc38e67fc590720
KataokaNagi <calm1836@gmail.com>
2022-05-10 06:14:47 +0900

    clean: mv common ui event to new class

--- URPTemplate/Assets/Scripts/UI/UiEvents.cs
+++ URPTemplate/Assets/Scripts/UI/UiEvents.cs
@@ -1,9 +1,22 @@
 /// <summary>
-/// @file     TitleUIEvent.cs
+/// @file     UiEvent.cs
 /// @brief    Define events type that need a parameter.
 /// @auther   Kataoka
-/// @date     2022-05-05 22:22:11
+/// @date     2022-05-10 06:12:44
 /// @version  1.0
 /// @history  add
 /// @see      [UnityにおけるMVPパターンについて](https://virtualcast.jp/blog/2019/11/mvp_pattern_on_unity/)
 /// </summary>
+
+using UnityEngine.Events;
+
+public sealed class BoolEvent : UnityEvent<bool>
+{
+}
+public sealed class IntEvent : UnityEvent<int>
+{
+}
+public sealed class FloatEvent : UnityEvent<float>
+{
+}
+

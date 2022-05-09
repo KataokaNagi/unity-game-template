9d53554e685f0ed61ea9bf8b36062b0acae9d1ef
KataokaNagi <calm1836@gmail.com>
2022-05-10 00:15:02 +0900

    add: common button ui state

---
+++ URPTemplate/Assets/Scripts/UI/ButtonUiState.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// @file     ButtonUiState.cs
+/// @brief    Common button ui state.
+/// @auther   Kataoka
+/// @date     2022-05-10 00:10:37
+/// @version  1.0
+/// @history  add
+/// </summary>
+
+public enum ButtonUiState
+{
+    Unhover = (1 << 0),
+    Hover = (1 << 1),
+    Down = (1 << 2),
+    Up = (1 << 3),
+    Range = 0b00001111
+}
